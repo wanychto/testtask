0b279db6e00813e40b7ad764c7503588d2072287
wanychto <smirnovaa2500@gmail.com>
2025-06-08 19:33:27 +0300

    Creating web page

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from trucks.views import truck_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', truck_list, name="truck_list"),
 ]
